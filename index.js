c86037c4ad9a9321756f9e1815c43f1aad44bbd8
Stephen Tomlinson <Stephen.emptycubicle@outlook.com>
2018-07-17 17:23:36 -0400

    Created Index.js that listens on port 8080. When the initial get is initiated it returns the wildcard results from two joined tables

---
+++ index.js
@@ -0,0 +1,41 @@
+/**
+ * This is Stephen and Will's FoodMaestro Hackathon app!
+ */
+
+
+const express = require('express');
+const app = express();
+
+// The Object below contains s
+const knex = require('knex')({
+    client: 'mssql',
+    connection: {
+        host: 'fmjob.database.windows.net',
+        user: 'fm_job',
+        password: 'Hackathon!2018',
+        database: 'data',
+        options: {
+            encrypt: true
+        }
+    }
+});
+
+app.get('/', function(req, res) {
+    // knex('product')
+    //     .join('productnutrition', 'product.ProductGUID', '=', 'productnutrition.ProductAttributeGUID')
+    //     .select('*').then((rows) => {
+    //             console.log();
+    //         });
+
+    knex.select('*').from('product').join('productnutrition', function() {
+        this.on('productnutrition.ProductID', '=', 'product.ProductID')
+        }).then(function(rows) {
+            console.log(rows);
+    });
+});
+
+
+app.listen(8080, () => console.log("App listening on port 8080"));
+
+
+
