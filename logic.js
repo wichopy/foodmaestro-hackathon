ede79a1d139e417491604bdd2b9b0082b4a36cc9
William Chou <william.chou@uhn.ca>
2018-07-17 17:27:49 -0400

    wip iron score.

--- logic.js
+++ logic.js
@@ -10,3 +10,14 @@ export const calculateSugarScore = (product) => {
     return -50
   }
 }
+
+export const calculateIronScore = (product) => {
+  const ironContent = product.per100gIron
+  if (ironContent >= 4.4) {
+    return 0
+  } else if (ironContent < 4.4 && ironContent >= 2.2) {
+    return -2.5
+  } else {
+    return -5
+  }
+}
